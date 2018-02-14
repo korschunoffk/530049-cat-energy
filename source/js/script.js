d370401230b9942a3771dffec2b46a2b33a221ad
korschunoffk <33195448+korschunoffk@users.noreply.github.com>
2018-02-14 16:41:01 +0300

    ретинизация и карта

---
+++ source/js/script.js
@@ -0,0 +1,31 @@
+﻿ymaps.ready(function() {
+ var myMap = new ymaps.Map('map', {
+   center: [59.938587, 30.322954],
+   zoom: 18
+  }, {
+   searchControlProvider: 'yandex#search'
+  }),
+  MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+   '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+  ),
+
+  myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+   hintContent: 'Собственный значок метки',
+  }, {
+   iconLayout: 'default#image',
+   // Своё изображение иконки метки.
+   iconImageHref: 'img/pin.png',
+   // Размеры метки.
+   iconImageSize: [124, 106],
+   // Смещение левого верхнего угла иконки относительно
+   // её "ножки" (точки привязки).
+   iconImageOffset: [-53, -113]
+  });
+ myMap.geoObjects
+  .add(myPlacemark);
+ myMap.setCenter([59.938617, 30.322985], 18);
+  width=document.body.clientWidth;
+  if (width>=1440){
+      myMap.setCenter([59.938707, 30.319369], 17);
+      }
+});
