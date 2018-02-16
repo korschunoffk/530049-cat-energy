31fbc54cc8fd7d0093c18337c581031a0e267661
korschunoffk <33195448+korschunoffk@users.noreply.github.com>
2018-02-16 22:06:54 +0300

    stylelint2

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,4 @@
-﻿ymaps.ready(function(){
+ymaps.ready(function () {
   var myMap = new ymaps.Map('map', {
     center: [59.938587, 30.322954],
     zoom: 18
@@ -19,51 +19,47 @@
     // её "ножки" (точки привязки).
     iconImageOffset: [-53, -113]
   });
-myMap.geoObjects.add(myPlacemark);
-myMap.setCenter([59.938617, 30.322985], 18);
+  myMap.geoObjects.add(myPlacemark);
+  myMap.setCenter([59.938617, 30.322985], 18);
   width=document.body.clientWidth;
   if (width>=1440){
-      myMap.setCenter([59.938707, 30.319369], 17);
+    myMap.setCenter([59.938707, 30.319369], 17);
   }
 });
 
-  var form = document.querySelector(".selection");
-  var catname = form.querySelector("[name=cat-name]");
-  var catweight = form.querySelector("[name=cat-weight]");
-  var email= form.querySelector("[name=email]");
-  var tel = form.querySelector("[name=tel]");
+var form = document.querySelector(".selection");
+var catname = form.querySelector("[name=cat-name]");
+var catweight = form.querySelector("[name=cat-weight]");
+var email= form.querySelector("[name=email]");
+var tel = form.querySelector("[name=tel]");
 
-  form.addEventListener("submit", function (evt) {
-    if (!catname.value) {
-      catname.classList.add("selection__red-border");
+form.addEventListener("submit", function (evt) {
+  if (!catname.value) {
+    catname.classList.add("selection__red-border");
     event.preventDefault();
-    }
-    else {
-      catname.classList.remove("selection__red-border");
-    }
-    if (!catweight.value) {
+  } else {
+    catname.classList.remove("selection__red-border");
+  }
+  if (!catweight.value) {
       catweight.classList.add("selection__red-border");
       event.preventDefault();
-    }
-    else {
-      catweight.classList.remove("selection__red-border");
-    }
-    if (!email.value) {
-      email.classList.add("selection__red-border");
-      email.classList.add("form-contacts__input--email-red");
-      event.preventDefault();
-    }
-    else {
-      email.classList.remove("selection__red-border");
-      email.classList.remove("form-contacts__input--email-red");
-    }
-    if (!tel.value) {
-      tel.classList.add("selection__red-border");
-      tel.classList.add("form-contacts__input--phone-red");
-      event.preventDefault();
-    }
-    else {
-      tel.classList.remove("selection__red-border");
-      email.classList.remove("form-contacts__input--phone-red");
-    }
+  } else {
+    catweight.classList.remove("selection__red-border");
+  }
+  if (!email.value) {
+    email.classList.add("selection__red-border");
+    email.classList.add("form-contacts__input--email-red");
+    event.preventDefault();
+  } else {
+    email.classList.remove("selection__red-border");
+    email.classList.remove("form-contacts__input--email-red");
+  }
+  if (!tel.value) {
+    tel.classList.add("selection__red-border");
+    tel.classList.add("form-contacts__input--phone-red");
+    event.preventDefault();
+  } else {
+    tel.classList.remove("selection__red-border");
+    email.classList.remove("form-contacts__input--phone-red");
+  }
 });
